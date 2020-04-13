2006f9270291dea5e3cc5796f3ea25f994d48b98
HyeockJinKim <kherootz@gmail.com>
2020-04-14 01:18:25 +0900

    Source code 크롤링 추가

--- popup.js
+++ popup.js
@@ -8,6 +8,11 @@ let get_url = (url) => fetch(url).then(data => data.text());
 
 let get_html = (url) => get_url(url).then(html => new DOMParser().parseFromString(html, "text/html"));
 
+let get_text = (infos, index) => infos.map(async data => {
+  let cell = await data;
+  return cell[index].textContent;
+});
+
 let get_textContent = (problem_infos, keyword) => problem_infos.map(async data => {
   let html = await data;
   let content = html.getElementById(keyword);
@@ -46,7 +51,43 @@ async function get_problem_infos(problems) {
   }
 }
 
+async function get_solution_infos(problems) {
+  let solution = problems.map(async problem =>
+    await get_html('https://www.acmicpc.net/status?from_mine=1&problem_id='+problem+'&user_id='+username));
+
+  let infos = get_cells(solution, 'status-table');
+  let numbers = get_text(infos, 0);
+  let success = get_text(infos, 3);
+  let memory = get_text(infos, 4);
+  let time = get_text(infos, 5);
+  let language = infos.map(async info => {
+    let temp = await info;
+    return temp[6].textContent.split('/')[0]
+  });
+  let length = get_text(infos, 7);
+
+  return {
+    numbers,
+    success,
+    memory,
+    time,
+    language,
+    length,
+  }
+}
+
+async function get_source(solutions) {
+  return solutions.map(async solution => {
+    let url = await solution;
+    return await get_url('https://www.acmicpc.net/source/download/' + url)
+  });
+}
+
 changeColor.onclick = async function(element) {
   let problems = await get_problems(username);
-  console.log(get_problem_infos(problems))
+  let problem_infos = get_problem_infos(problems)
+  let solutions = get_solution_infos(problems);
+  let sols = await solutions;
+  let source = get_source(sols.numbers);
+  console.log(source)
 };
